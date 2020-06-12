27b5c6fb6f403aeb58c0e65b824dce3b3bced9dd
Abukhalil95 <yahya_abukhaleel@hotmail.com>
2020-06-12 17:49:07 +0300

    css and bit more content

--- js/prompt.js
+++ js/prompt.js
@@ -11,91 +11,95 @@ var questions = [
         ['Reading books?', 'Correct! I like reading technical books, only when I am not tired.','Nope.'],
         ['Hiking perhaps?', 'Correct! I love hiking, and having different perspectives of a view.', 'Nope.'],
         ['Do I like coding', 'Nope.', 'Correct! I don\'t \"like\" coding, but I love building stuff!.']
-    ];
-var movieList = 'Copy my favorite movie into the answer box: \
-A.I. Artificial Intelligence, Contact, \
-Grave of the fireflies, The Shawshank Redemption, Kingdom of Heaven: Director\'s Cut, Whiplash, \
-The Grand Budapest Hotel, Spirited Away, Man on Wire, Inception';
+];
+var movieList = 'Guess my favorite movie name or number into the answer:\n1. A.I. Artificial Intelligence\n2. Contact\n\
+3. Grave of the fireflies\n4. The Shawshank Redemption\n5. Kingdom of Heaven: Director\'s Cut\n6. Whiplash\n\
+7. The Grand Budapest Hotel\n8. Spirited Away\n9. Man on Wire\n10. Inception';
 
 
- function myName(){
+function myName() {
         var name = prompt('So, what should I call you', 'Harry Potter');
         var txt = "Hello! " + name + ". How much do you know about me?";
         document.getElementById("name").innerHTML = txt;
- }
+}
 
-
- function askMe(){
+function askMe() {
         for (var i=0; i<5; i++) {
                 answer = prompt(questions[i][0], "y/n").toLowerCase();
-                while (answer !== "y" && answer !== "n"){
-                        answer = prompt(resubmit).toLowerCase();
+                while (answer !== "y" && answer !== "n" && answer !== "yes" && answer !== "no"){
+                        answer = prompt(resubmit + "\n" + questions[i][0]).toLowerCase();
+                }
+                if (answer == "y" || answer == "yes"){
+                        alert(questions[i][1]);
+                        if(!(questions[i][1]=="Nope.")){
+                                score++;
                         }
-                        if (answer == "y"){
-                                alert(questions[i][1]);
-                                if(!(questions[i][1]=="Nope.")){
-                                        score++;
-                                }
-                        }   else {
-                                alert(questions[i][2]);
-                                if(!(questions[i][2]=="Nope.")){
-                                        score++;
-                                }
+                }   else {
+                        alert(questions[i][2]);
+                        if(!(questions[i][2]=="Nope.")){
+                                score++;
                         }
+                }
         }
-
- }
+}
 
 function guessNum() {
-        var numGuess = prompt('Guess a number between 1-100.');
+        var numGuess = prompt('Here is a fun game!\nGuess a number between 1-100 at most 8 times and try to almost halve or double the guess each time.\nYou will always get this right.');
         var myNum = Math.floor(Math.random() * 100) + 1;
-        for (var i=0; i<=3; i++){
-                if (i<3){
+        var numGuessString = '';
+        for (var i=0; i<=7; i++){
+                if (i<7){
                         if (numGuess==myNum){
                                 alert(`Correct`);
                                 score++;
                                 break;
                         }
                         if (numGuess>myNum){
-                                numGuess = prompt('Too large, guess again between 1-100.');
+                                numGuessString += numGuess + '  '
+                                numGuess = prompt('Too large, guess again between 1-100. Last answers: ' + numGuessString);
                         }
                         if (numGuess<myNum){
-                                numGuess = prompt('Too small, guess again between 1-100.');
+                                numGuessString += numGuess + '  '
+                                numGuess = prompt('Too small, guess again between 1-100. Last answers: ' + numGuessString);
                         }
                 } else {
                         alert(`Too late, the answer is: ` + myNum)
                 }
-        }
+}
 
 }
-function guessMovie(){
-        var ansGuess = prompt(movieList);
+function guessMovie() {
+        var ansGuess = prompt(movieList).toLowerCase();
+        var ansValue = 'shawshank redemption';
         for (var i=0; i<=6; i++){
                 if (i<6){
-                        if (ansGuess == 'Grave of the fireflies'){
+                        if (ansValue.includes(ansGuess)|| ansGuess == 4 ){
                                 alert(`Correct`)
                                 score++;
                                 break;
                         }   else {
                                 alert(`Nope`);
                                 ansGuess = prompt(movieList);
-                
                         }
                 } else {
-                        alert(`Too late, the answer is: Grave of the fireflies`);
+                        alert(`Too late, the answer is: The Shawshank Redemption`);
                 }
         }
-
 }
 
-function myScore(){
-        var endscore = "You, " + name + " scored " + score + "/7 in this pop quiz.";
+function myScore() {
+        var btn = document.createElement("BUTTON");
+        btn.innerHTML = "Click to restart!";
+        var endscore = "You, " + name + " scored " + score + "/7 in this pop quiz.<br><br>";
+        document.getElementById('endscore').innerHTML = '';
         document.getElementById("endscore").innerHTML = endscore;
+        document.getElementById('endscore').appendChild(btn);
 }
 
-
-myName();
-askMe();
-guessNum();
-guessMovie();
-myScore();
+function startQuiz() {
+        myName();
+        askMe();
+        guessNum();
+        guessMovie();
+        myScore();
+}
