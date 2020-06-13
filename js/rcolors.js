68d66f1f145cee93424282793937dac768b04196
Abukhalil95 <yahya_abukhaleel@hotmail.com>
2020-06-13 20:51:27 +0300

    added randomized color pallete onclick

---
+++ js/rcolors.js
@@ -0,0 +1,14 @@
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+
+
+function setRandomColor(clicked_id) {
+  document.getElementById(clicked_id).style.background = getRandomColor();
+}
